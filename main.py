305d732c30811c963bc884bf6d62e29a1733498d
Akozynetsj <Kozin_dn78@student.itstep.org>
2023-06-01 13:44:41 +0300

    ex 3

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-def get_tables(url):
+def get_elements_with_class(url, class_name):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    tables = soup.find_all('table')
-    for table in tables:
-        print(table.prettify())
+    elements = soup.find_all(class_=class_name)
+    for element in elements:
+        print(element)
 url = 'https://www.example.com'
-get_tables(url)
+class_name = 'example-class'
+get_elements_with_class(url, class_name)
